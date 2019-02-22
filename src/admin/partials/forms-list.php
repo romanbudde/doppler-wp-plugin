6fd982595cbbca7ff0db1e42c17a51fa3170f8b8
Hernan Galvan <hgalvan@ms.local>
2019-02-22 11:54:56 -0300

    Agregadas funciones de tranduccion en form list. Agregado link 'edit' a form list.

--- src/admin/partials/forms-list.php
+++ src/admin/partials/forms-list.php
@@ -1,4 +1,4 @@
-<h1>Forms List</h3>
+<h1><?php echo esc_html('Forms List', 'doppler-form')?></h1>
 <div class="dplr">
   <div class="panel">
     <div class="">
@@ -6,10 +6,10 @@
         <thead>
           <tr>
             <th class="cb"><input type="checkbox" name="" value=""></th>
-            <th class="col-id">Id</th>
-            <th class="col-title">Title</th>
-            <th class="col-listname">List name</th>
-            <th class="col-listid">List Id</th>
+            <th class="col-id"><?php echo esc_html('Id', 'doppler-form')?></th>
+            <th class="col-title"><?php echo esc_html('Title', 'doppler-form')?></th>
+            <th class="col-listname"><?php echo esc_html('List Name', 'doppler-form')?></th>
+            <th class="col-listid"><?php echo esc_html('List Id', 'doppler-form')?></th>
           </tr>
         </thead>
         <tbody>
@@ -21,7 +21,8 @@
             <td>
               <a href="<?php echo str_replace('[FORM_ID]', $form->id , $edit_form_url); ?>" class="bold"> <?php echo $form->title; ?></a>
               <div class="column-actions">
-                <a href="<?php echo str_replace('[FORM_ID]', $form->id , $delete_form_url); ?>">Delete</a>
+                <a href="<?php echo str_replace('[FORM_ID]', $form->id , $edit_form_url); ?>"><?php echo esc_html('Edit', 'doppler-form')?></a> |
+                <a href="<?php echo str_replace('[FORM_ID]', $form->id , $delete_form_url); ?>"><?php echo esc_html('Delete', 'doppler-form')?></a>
               </div>
             </td>
             <td><?php echo $dplr_lists_arr[$form->list_id] ?></td>
@@ -34,4 +35,4 @@
   </div>
 </div>
 
-<a href="<?php echo $create_form_url; ?>" class="button button-primary">Crear Formulario</a>
+<a href="<?php echo $create_form_url; ?>" class="button button-primary"><?php echo esc_html('New Form', 'doppler-form')?></a>
