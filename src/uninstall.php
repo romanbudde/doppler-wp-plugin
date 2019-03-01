382cb1bb105d3522459075e52a22f9984776919d
Hernan Galvan <hgalvan@ms.local>
2019-03-01 12:49:10 -0300

    Se desinstalas opciones y tablas al eliminar el plugin.

--- src/uninstall.php
+++ src/uninstall.php
@@ -1,31 +1,22 @@
 <?php
 
-/**
- * Fired when the plugin is uninstalled.
- *
- * When populating this file, consider the following flow
- * of control:
- *
- * - This method should be static
- * - Check if the $_REQUEST content actually is the plugin name
- * - Run an admin referrer check to make sure it goes through authentication
- * - Verify the output of $_GET makes sense
- * - Repeat with other user roles. Best directly by using the links/query string parameters.
- * - Repeat things for multisite. Once for a single site in the network, once sitewide.
- *
- * This file may be updated more in future version of the Boilerplate; however, this is the
- * general skeleton and outline for how the file should work.
- *
- * For more information, see the following discussion:
- * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
- *
- * @link       http://example.com
- * @since      1.0.0
- *
- * @package    Plugin_Name
- */
-
-// If uninstall not called from WordPress, then exit.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
+}
+
+if( $_REQUEST['slug']==='doppler-form' ){
+
+	global $wpdb;
+
+	$option_name = 'dplr_settings';
+	delete_option($option_name);
+	delete_site_option($option_name);
+
+	$option_name = 'widget_dplr_form_widget';
+	delete_option($option_name);
+	delete_site_option($option_name);
+
+	$wpdb->query( "DROP TABLE IF EXISTS ".$wpdb->prefix."dplr_field_settings, ".$wpdb->prefix."dplr_field, 
+				  ".$wpdb->prefix."dplr_form_settings, ".$wpdb->prefix."dplr_form" );
+
 }
